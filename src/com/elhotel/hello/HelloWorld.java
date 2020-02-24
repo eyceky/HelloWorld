688565e95fd079dfe924ff2021fb55382e321707
eyceky <eyceky@139.com>
2020-02-24 22:25:04 +0800

    更改：HelloWorld->你好，世界

--- src/com/elhotel/hello/HelloWorld.java
+++ src/com/elhotel/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package com.elhotel.hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("你好，世界");
     }
 }
